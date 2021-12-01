f8570543701b20af0780ce0c8c72c68b244c49e7
nimbl0 <nimble0@protonmail.com>
2021-12-01 15:57:17 +0100

    i made it a little bit faster

--- 2021/day1/partTwo.js
+++ 2021/day1/partTwo.js
@@ -9,23 +9,22 @@ let contentArr = String(fs.readFileSync(path.join(__dirname, "input.txt"))).spli
 const pStart = performance.now();
 
 function solution(arr) {
-    let largerMeasurements = 0;
-    let threeMeasurementSums = [];
-
+    let lastSum = 0;
+    let count = 0;
+    
     for(let i = 0; i < arr.length; i++) {
-        let nums = [arr[i], arr[i+1], arr[i+2]];
-        let threeSum = nums[0] + nums[1] + nums[2];
-        threeMeasurementSums.push(threeSum);
-    }
+        let threeSum = arr[i] + arr[i+1] + arr[i+2];
 
-    for(let i = 0; i < threeMeasurementSums.length; i++) {
-        if(i == 0) continue;
-        if(threeMeasurementSums[i] > threeMeasurementSums[i-1]) {
-            largerMeasurements++;
+        if(lastSum <= 0) lastSum = threeSum;
+
+        if(threeSum > lastSum) {
+            count++;
         }
+        
+        lastSum = threeSum;
     }
 
-    return largerMeasurements;
+    return count;
 }
 
 let res = solution(contentArr);
