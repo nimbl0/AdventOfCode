792fc33c55951829297606403e6cf89470af95fd
nimbl0 <nimble0@protonmail.com>
2021-12-02 07:52:04 +0100

    more efficient day 1

--- 2021/Day_01/part_1.js
+++ 2021/Day_01/part_1.js
@@ -10,10 +10,11 @@ const pStart = performance.now();
 
 function solution(arr) {
     let largerMeasurements = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(i == 0) continue;
-        if(arr[i] > arr[i-1]) largerMeasurements++;
-    }
+    
+    arr.forEach((element, i) => {
+        if(element > arr[i-1]) largerMeasurements++;
+    });
+
     return largerMeasurements;
 }
 
