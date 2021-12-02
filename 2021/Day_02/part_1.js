0050eab0b462265b024bd8ea4cc7802aa1dc68f6
nimbl0 <nimble0@protonmail.com>
2021-12-02 07:51:55 +0100

    day 2

---
+++ 2021/Day_02/part_1.js
@@ -0,0 +1,34 @@
+"use strict";
+
+let fs = require("fs");
+let path = require("path");
+let { performance } = require("perf_hooks");
+
+const data = String(fs.readFileSync(path.join(__dirname, "input.txt"))).split('\n');
+
+const pStart = performance.now();
+
+function solution(arr) {
+    let depth = 0;
+    let forward = 0;
+
+    arr.forEach(element => {
+        let splitted = element.split(" ");
+        if(splitted[0] == "forward") {
+            forward += Number(splitted[1]);
+        } else if(splitted[0] == "up") {
+            depth -= Number(splitted[1]);
+        } else if(splitted[0] == "down") {
+            depth += Number(splitted[1]);
+        }
+    });
+
+    return depth * forward;
+}
+
+let res = solution(data);
+
+const pEnd = performance.now();
+
+console.log(!res ? "No match" : "Result: " + res);
+console.log(pEnd - pStart);
